138839806fc9dd1bb66f3a7a07f215cacc974a40
Nathan L <nlandmanc@gmail.com>
2022-08-17 16:10:50 +0100

    committing!

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Main from "./components/Main";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Main />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
